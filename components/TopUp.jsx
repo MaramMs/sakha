b42319b56f5c02020f91f4e7d76fab5b4ee4b442
Maran.I.Msabeh <maramismail2015@gmail.com>
2023-07-08 13:11:55 +0300

    edit

--- components/TopUp.jsx
+++ components/TopUp.jsx
@@ -1,3 +1,4 @@
+import { Button } from "antd";
 import React from "react";
 
 const TopUp = ({item}) => {
@@ -8,7 +9,7 @@ const TopUp = ({item}) => {
             <div className="flex flex-col justify-between gap-[8px] w-[144px] h-[83px] absolute top-[77px] left-[13px] topUpItem">
               <p className="font-[900] text-[#fff] text-[14px] w-[303px]">{item.title}</p>
 
-              <button className="bg-[#669640] px-[16px] py-[14px] rounded-[4px] h-[31px] flex justify-center items-center border-none text-[#fff] font-[900]">Discover Now!</button>
+              <Button className="bg-[#669640] px-[16px] py-[14px] rounded-[4px] h-[31px] flex justify-center items-center border-none text-[#fff] font-[900]">Discover Now!</Button>
             </div>
           </div>
     
