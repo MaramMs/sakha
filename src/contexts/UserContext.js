4c40667964ba657bcf8702e17da8ba1f60430220
Maran.I.Msabeh <maramismail2015@gmail.com>
2023-07-10 12:41:38 +0300

    add userInfo to userContext

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,16 +1,22 @@
-'use client'
+"use client";
 const { createContext, useState, useEffect } = require("react");
 
 export const UserContext = createContext();
 
-export function UserProvider({children}) {
-    const [user,setUser] = useState({
-        email:'',
-        password:''
-    });
-    return (
-        <UserContext.Provider value={{user,setUser}}>
-            {children}
-        </UserContext.Provider>
-    )
+export function UserProvider({ children }) {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+    name: "",
+    mobile: "",
+    gender: "",
+    country_id: "",
+    city_id: "",
+  });
+  const [userInfo ,setUserInfo] = useState({})
+  return (
+    <UserContext.Provider value={{ user, setUser,setUserInfo ,userInfo}}>
+      {children}
+    </UserContext.Provider>
+  );
 }
